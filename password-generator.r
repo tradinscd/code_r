d4f500bb0dc18b18028cd934e12ba8d99abd52e2
Chris R. Albon <3179476+tradinscd@users.noreply.github.com>
2013-11-07 20:33:05 -0500

    Added password generation snippit

---
+++ password-generator.r
@@ -0,0 +1,21 @@
+# Password Generator
+# Credit to: http://ryouready.wordpress.com/2008/12/18/generate-random-string-name/
+# Credit to: http://stats.stackexchange.com/questions/7900/generate-random-strings-based-on-regular-expressions-in-r
+
+# enter the url here:
+url <- "http://chrisralbon.com"
+
+# password generating function
+password <- function(x)
+{
+  length=nchar(x)
+  random.string <- c(1:1)
+  for (i in 1:1)
+  {
+    random.string[i] <- paste(sample(c(0:9, letters, LETTERS), length, replace=TRUE), collapse="")
+  }
+  return(random.string)
+}
+
+# generate the password
+password(url)
