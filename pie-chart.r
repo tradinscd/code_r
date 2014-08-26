86471b34fc50bb1efad7d0c20dcb51ed565e9683
chrisalbon <3179476+tradinscd@users.noreply.github.com>
2014-08-25 17:13:16 -0700

    pie chart changes

--- pie-chart.r
+++ pie-chart.r
@@ -2,10 +2,10 @@
 # Original source: http://www.statmethods.net/graphs/pie.html
 
 # Create some casualty numbers
-slices <- c(10, 12, 4, 16, 8)
+slices <- c(4, 6, 1, 0, 0, 0, 5)
 
 # Create labels that correspond to the casualty numbers
-lbls <- c("US", "UK", "Australia", "New Zealand", "France")
+lbls <- c("CrisisNET", "V3", "V2", "Crowdmap", "Ping", "SMSSync", "Other")
 
 # Create Percents For Each Slice
 pct <- round(slices/sum(slices)*100)
@@ -17,4 +17,4 @@ lbls <- paste(lbls, pct)
 lbls <- paste(lbls,"%",sep="")
 
 # Create a pie chart with labels, with each slice colored by the terrain color pallete
-pie(slices,labels = lbls, col=terrain.colors(length(lbls)), main="Casualties") 
+pie(slices,labels = lbls, col=terrain.colors(length(lbls)), main="Main Ushahidi Blog Posts") 
